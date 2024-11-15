4d6d511b8f0a8971b711d0b4218bf41eb47f0f31
castro <castro.nunes@aluno.senai.br>
2024-11-15 14:03:31 -0300

    Inicio do projeto

---
+++ LeLivros/src/main/java/com/mycompany/lelivros/LeLivros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lelivros;
+
+/**
+ *
+ * @author edmun
+ */
+public class LeLivros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
